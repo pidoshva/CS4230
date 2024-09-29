034da0d26ed2889929c5ade11f4a2e6c5cbb20a3
columcille <trajanclark@gmail.com>
2024-09-29 09:35:58 -0600

    updated employee.py
    I had my own updates, I'll compare and push them.

--- employees.py
+++ employees.py
@@ -46,7 +46,31 @@ class Supervisor(Employee):
             print(str.format("Unable to hire new workers. Max workers for Supervisor {} already reached",self.name))
 
     def layoff_employee(self, name):
-        pass  # Logic for laying off to be implemented later
+        found = False
+        layoff = None
+        for worker in self.subordinates:
+            if worker.name == name:
+                found = True
+                layoff = self.subordinates.pop(worker)
+            
+        if not found:
+            print(str.format("Worker {} not found", name))
+
+        overseer = self.overseer
+        while overseer is not None:
+            for supervisor in overseer.subordinates:
+                if isinstance(supervisor, Supervisor) and supervisor.name != self.name:
+                    if len(supervisor.subordinates) < supervisor.maxSubordinates:
+                        supervisor.subordinates.append(layoff)
+                        layoff.setOverseer(supervisor)
+                        return
+                elif isinstance(supervisor, VicePresident):
+                    for worker in supervisor.subordinates:
+                        if len(worker.subordinates) < worker.maxSubordinates:
+                            worker.subordinates.append(layoff)
+                            layoff.setOverseer(worker)
+                            return
+            overseer = overseer.overseer
 
     def fire_employee(self, worker):
         self.subordinates.remove(worker)
@@ -55,10 +79,13 @@ class Supervisor(Employee):
         self.overseer.fire_employee(self)
 
     def show_details(self):
-        print(f"Supervisor: {self.name}")
-        for subordinate in self.subordinates:
-            subordinate.show_details()
-        pass  # Display logic to be implemented later
+        if self.name != "":
+            print(f"Supervisor: {self.name}")
+            for subordinate in self.subordinates:
+                subordinate.show_details()
+        else:
+            for subordinate in self.subordinates:
+                subordinate.show_details()
 
 # Composite: VicePresident class (can have supervisors)
 class VicePresident(Employee):
@@ -72,33 +99,87 @@ class VicePresident(Employee):
         self.overseer = overseer
 
     def promote_employee(self, name):
+        vacancy = False
+        if len(self.subordinates) < self.maxSubordinates:
+            vacancy = True
+        else:
+            for supervisor in self.subordinates:
+                if supervisor.name == "":
+                    vacancy = True
+
+        if vacancy:
+            for supervisor in self.subordinates:
+                for worker in supervisor.subordinates:
+                    if worker.name == name:
+                        supervisor.subordinates.remove(worker)
+                        self.hire_employee(worker)
+            
         pass  # Logic for promoting to be implemented later
 
     def hire_employee(self, supervisor):
         if supervisor.name == "":
             self.subordinates.append(supervisor)
+            return
         elif len(self.subordinates) < self.maxSubordinates:
             self.subordinates.append(supervisor)
             supervisor.setOverseer(self)
+            return
         else:
+            for supervisor in self.subordinates:
+                if supervisor.name == "":
+                    supervisor.name = supervisor.name
+                    return
             print(str.format("Unable to hire new supervisors. Max workers for Vice President {} already reached", self.name))
 
-    def layoff_employee(self):
-        pass  # Logic for laying off to be implemented later
+    def layoff_employee(self, name):
+        found = False
+        layoff = None
+        for supervisor in self.subordinates:
+            if supervisor.name == name:
+                found = True
+                layoff = Supervisor(name)
+                supervisor.name = ""
+            
+        if not found:
+            print(str.format("Worker {} not found", name))
+
+        overseer = self.overseer
+        while overseer is not None:
+            for vp in overseer.subordinates:
+                if vp.name != self.name:
+                    if len(vp.subordinates) < vp.maxSubordinates:
+                        vp.subordinates.append(layoff)
+                        layoff.setOverseer(vp)
+                        return
+                    else:
+                        for supervisor in vp.subordinates:
+                            if supervisor.name == "":  # If supervisor is not hired
+                                supervisor.name = layoff.name
+                                return
+            overseer = overseer.overseer
 
     def fire_employee(self, supervisor):
-        self.subordinates.remove(supervisor)
+        for person in self.subordinates:
+            if person.name == supervisor.name:
+                person.name = ""
+                return
     
     def transfer_employee(self, name):
+        transfer = None
+        found = False
         pass  # Logic for transferring to be implemented later
 
     def quit(self):
         self.overseer.fire_employee(self)
 
     def show_details(self):
-        print(f"Vice President: {self.name}")
-        for subordinate in self.subordinates:
-            subordinate.show_details()
+        if self.name != "":
+            print(f"Vice President: {self.name}")
+            for subordinate in self.subordinates:
+                subordinate.show_details()
+        else:
+            for subordinate in self.subordinates:
+                subordinate.show_details()
         pass  # Display logic to be implemented later
 
 # Composite: President class (can have vice presidents)
@@ -128,7 +209,10 @@ class President(Employee):
         pass  # Logic for laying off to be implemented later
 
     def fire_employee(self, vicePresident):
-        self.subordinates.remove(vicePresident)
+        for person in self.subordinates:
+            if person.name == vicePresident.name:
+                person.name = ""
+                return
 
     def show_details(self):
         print(f"President: {self.name}")
